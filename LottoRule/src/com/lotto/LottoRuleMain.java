c6e1cb5a43469342aba3eaf25414bb3928f7a1bf
준성 조 <anabasis01@gmail.com>
2014-06-10 15:34:35 +0900

    Sample Source

--- LottoRule/src/com/lotto/LottoRuleMain.java
+++ LottoRule/src/com/lotto/LottoRuleMain.java
@@ -1,10 +1,41 @@
 package com.lotto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class LottoRuleMain {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+		Connection connection = null;
+		ResultSet resultSet = null;
+		Statement statement = null;
 
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+			connection = DriverManager
+					.getConnection("jdbc:derby:/Users/junseongjo/git/eclipse_workspace/LottoRule/DB_SQL/DBDIC.dat;create=true");
+			statement = connection.createStatement();
+			resultSet = statement
+					.executeQuery("SELECT EMPNAME FROM EMPLOYEEDETAILS");
+			while (resultSet.next()) {
+				System.out.println("EMPLOYEE NAME : "
+						+ resultSet.getString("EMPNAME"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				resultSet.close();
+				statement.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
